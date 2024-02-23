bf7d56d5c029a78a606ce816183ea528f846ecc6
Violangige02 <violangige@gmail.com>
2024-02-24 01:52:44 +0300

    initial commit

---
+++ app/src/main/java/com/example/welcome/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.welcome;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onButtonClick(View view) {
+        // Method to handle button click event
+        Toast.makeText(this, "Welcome to my app!", Toast.LENGTH_SHORT).show();
+    }
+}
